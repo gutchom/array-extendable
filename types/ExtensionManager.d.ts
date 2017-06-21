eec681e40aa3d5789914bf2a482a373332e42033
gutchom <yskz.tg@gmail.com>
2017-06-21 13:47:52 +0900

    Reconsider about a method to manage the extensions and its implements.

--- types/ExtensionManager.d.ts
+++
@@ -1,6 +0,0 @@
-declare class ExtensionManager {
-  constructor(proto: any)
-  readonly availables: string[]
-  attach(...extensions: Extension[]): void
-  detach(...names: string[]): void
-}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,47 @@
-interface Array<T> {
-  head: T
-  last: T
-  nth(n: number): T
-  unique(predicate?: (current: T, first: T) => boolean): T[]
-  duplicates(predicate?: (a: T, b: T) => boolean): T[]
+interface Named {
+  name: string
 }
 
-interface Extendable {
-  readonly availables: string[]
-  attach(...extensions: Extension[]): void
-  detach(...names: string[]): void
+interface DescriptorBase {
+  configurable: boolean
+  enumerable: boolean
+}
+
+interface DataDescriptor {
+  value: any
+  writable: boolean
+}
+
+interface GetterDescriptor {
+  get: () => any
+}
+
+interface SetterDescriptor {
+  set: (val: any) => void
+}
+
+type AccessorDescriptor = GetterDescriptor & SetterDescriptor
+
+type VariousDescriptor = DataDescriptor | GetterDescriptor | SetterDescriptor | AccessorDescriptor
+
+type Descriptor = DescriptorBase & VariousDescriptor
+
+type Extension = Function | VariousDescriptor & Named
+
+declare module 'array-extendable' {
+  export function activate(...extensions: Extension[]): void
+
+  export const last: AccessorDescriptor
+  export const nth: GetterDescriptor
+  export function uniques<T>(distinguish?: (former: T, latter: T) => boolean): T[]
+  export function dupes<T>(distinguish?: (former: T, latest: T) => boolean): T[]
+  export function dedupe<T>(distinguish?: (former: T, latest: T) => boolean): T[]
+}
+
+interface Array<T> {
+  last: T
+  nth: {[n: number]: T}
+  uniques<T>(distinguish?: (former: T, latter: T) => boolean): T[]
+  dupes<T>(distinguish?: (former: T, latest: T) => boolean): T[]
+  dedupe<T>(distinguish?: (former: T, latest: T) => boolean): T[]
 }
