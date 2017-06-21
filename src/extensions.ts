953d3fd76ad6be95135ec89a3bcb090b06bb3598
gutchom <yskz.tg@gmail.com>
2017-06-21 13:50:38 +0900

    Rewrite the extensions and omit a manager.

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,43 +1,44 @@
-const config = {
-  configurable: true,
+export const last: AccessorDescriptor & Named = {
+  name: 'last',
+  get: function() {return this[this.length - 1]},
+  set: function(val: any) {this[this.length - 1] = val},
 }
 
-export function head(): Descriptor {
-  return Object.assign({}, config, {
-    get: function() {return this[0]},
-    set: function(val: any) {this[0] = val},
+export const nth: GetterDescriptor & Named = {
+  name: 'nth',
+  get: function(): any[] {
+    return new Proxy(this, {
+      get: function(array, index: string) {
+        return array[index[0] === '-' ? array.length + parseInt(index, 10) : index]
+      },
+      set: function(array, index: string, value) {
+        array[index[0] === '-' ? array.length + parseInt(index, 10) : index] = value
+        return true
+      },
+    })
+  }
+}
+
+export function uniques<T>(distinguish?: (former: T, latter: T) => boolean): T[] {
+  return this.filter((former: T, index: number, self: T[]) => {
+    return self.filter(latter => {
+      return distinguish ? distinguish(former, latter) === false : former === latter
+    }).length === 1
   })
 }
 
-export function last(): Descriptor {
-  return Object.assign({}, config, {
-    get: function() {return this[this.length - 1]},
-    set: function(val: any) {this[this.length - 1] = val},
+export function dupes<T>(distinguish?: (former: T, latest: T) => boolean): T[] {
+  return this.filter((former: T, index: number, self: T[]) => {
+    return self.filter(latest => {
+      return distinguish ? distinguish(former, latest) === false : former === latest
+    }).length > 1
   })
 }
 
-export function nth(): Descriptor {
-  return Object.assign({}, config, {
-    value: function nth(n: number) {return n < 0 ? this[this.length + n] : this[n]},
-  })
-}
-
-export function unique(): Descriptor {
-  return Object.assign({}, config, {
-    value: function dedupe<T>(predicate?: (current: T, first: T) => boolean): T[] {
-      return this.filter((current: T, index: number, self: T[]) => {
-        return index <= self.findIndex(first => {
-          return predicate ? predicate(current, first) === true : current === first
-        })
-      })
-    },
-  })
-}
-
-export function duplicates(): Descriptor {
-  return Object.assign({}, config, {
-    value: function duplicate<T>(predicate?: (a: T, b: T) => boolean): T[] {
-      return this.filter((a: T) => this.filter((b: T) => predicate ? predicate(a, b) === true : a === b).length >= 2, this)
-    },
+export function dedupe<T>(distinguish?: (former: T, latest: T) => boolean): T[] {
+  return this.filter((latest: T, index: number, self: T[]) => {
+    return index <= self.findIndex(former => {
+      return distinguish ? distinguish(former, latest) === false : former === latest
+    })
   })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import ExtensionManager from './ExtensionManager'
+function activate(...extensions: Extension[]): void {
+  extensions.forEach(extension => {
+    const descriptor: Descriptor = typeof extension === 'function'
+        ? {configurable: true, enumerable: false, value: extension, writable: false}
+        : Object.assign({configurable: true, enumerable: false}, extension)
+    Object.defineProperty(Array.prototype, extension.name, descriptor)
+  })
+}
 
-const extension = new ExtensionManager(Array.prototype)
-
-export default extension
+export default activate
 export * from './extensions'
